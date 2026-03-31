a5c10916edc759932334d16a126df035445139ec
wendercsa <wenderc.silva@outlook.com>
2026-03-31 00:42:23 -0300

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,10 +1,23 @@
 function baterPonto(tipo) {
+  const nome = localStorage.getItem('nome');
+  const usuario = localStorage.getItem('usuario');
+
+  if (!nome || !usuario) {
+    alert("Erro: usuário não identificado");
+    window.location.href = "login.html";
+    return;
+  }
+
   fetch('/bater-ponto', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
     },
-    body: JSON.stringify({ tipo })
+    body: JSON.stringify({
+      tipo,
+      nome,
+      usuario
+    })
   })
   .then(res => res.text())
   .then(msg => alert(msg))
